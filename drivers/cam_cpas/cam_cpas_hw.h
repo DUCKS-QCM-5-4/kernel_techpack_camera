5458bb6afb9355b5013d6c1458005ffe4ba97fe3
Pavel Dubrova <pashadubrova@gmail.com>
2025-09-29 12:50:01 +0300

    common: Add support for interconnect bus scaling
    Add support for interconnect bw voting based on source
    and destination bus ids.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- drivers/cam_cpas/cam_cpas_hw.h
+++ drivers/cam_cpas/cam_cpas_hw.h
@@ -16,6 +16,7 @@
 #include "cam_cpas_api.h"
 #include "cam_cpas_hw_intf.h"
 #include "cam_common_util.h"
+#include "cam_soc_bus.h"
 
 #define CAM_CPAS_MAX_CLIENTS 30
 #define CAM_CPAS_INFLIGHT_WORKS 5
@@ -113,31 +114,21 @@ struct cam_cpas_client {
 /**
  * struct cam_cpas_bus_client : Bus client information
  *
- * @src: Bus master/src id
- * @dst: Bus slave/dst id
- * @pdata: Bus pdata information
- * @client_id: Bus client id
- * @num_usecases: Number of use cases for this client
- * @num_paths: Number of paths for this client
- * @curr_vote_level: current voted index
- * @dyn_vote: Whether dynamic voting enabled
- * @lock: Mutex lock used while voting on this client
  * @valid: Whether bus client is valid
- * @name: Name of the bus client
+ * @lock: Mutex lock used while voting on this client
+ * @dyn_vote: Whether dynamic voting enabled
+ * @curr_vote_level: current voted index
+ * @common_data: Common data fields for bus client
+ * @soc_bus_client: Bus client private information
  *
  */
 struct cam_cpas_bus_client {
-	int src;
-	int dst;
-	struct msm_bus_scale_pdata *pdata;
-	uint32_t client_id;
-	int num_usecases;
-	int num_paths;
-	unsigned int curr_vote_level;
-	bool dyn_vote;
-	struct mutex lock;
 	bool valid;
-	const char *name;
+	struct mutex lock;
+	bool dyn_vote;
+	unsigned int curr_vote_level;
+	struct cam_soc_bus_client_common_data common_data;
+	void *soc_bus_client;
 };
 
 /**

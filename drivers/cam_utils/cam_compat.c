2033e53de86f5e3150e08dd77f53acbe5ae04aa9
Pavel Dubrova <pashadubrova@gmail.com>
2025-09-29 12:50:01 +0300

    cam_smmu: Use ioremap APIs to map the FW region
    The camera firmware buffer is located in a reserved-memory
    region described in device tree. This is not system RAM
    managed by the DMA API, so using dma_alloc_coherent() was
    incorrect.
    
    Instead, the driver should map the reserved memory into the
    kernel address space with ioremap_wc(). This ensures
    correct access semantics for device-provided memory and
    avoids misusing the DMA coherent allocator.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- drivers/cam_utils/cam_compat.c
+++ drivers/cam_utils/cam_compat.c
@@ -11,6 +11,45 @@
 #include "cam_cpas_api.h"
 #include "camera_main.h"
 
+int cam_reserve_icp_fw(struct cam_fw_alloc_info *icp_fw, size_t fw_length)
+{
+	struct device_node *of_node;
+	struct device_node *mem_node;
+	struct resource     res;
+	int rc = 0;
+
+	of_node = (icp_fw->fw_dev)->of_node;
+	mem_node = of_parse_phandle(of_node, "memory-region", 0);
+	if (!mem_node) {
+		rc = -ENOMEM;
+		CAM_ERR(CAM_SMMU, "FW memory carveout not found");
+		goto end;
+	}
+	rc = of_address_to_resource(mem_node, 0, &res);
+	of_node_put(mem_node);
+	if (rc < 0) {
+		CAM_ERR(CAM_SMMU, "Unable to get start of FW mem carveout");
+		goto end;
+	}
+	icp_fw->fw_hdl = res.start;
+	icp_fw->fw_kva = ioremap_wc(icp_fw->fw_hdl, fw_length);
+	if (!icp_fw->fw_kva) {
+		CAM_ERR(CAM_SMMU, "Failed to map the FW.");
+		rc = -ENOMEM;
+		goto end;
+	}
+
+	memset_io(icp_fw->fw_kva, 0, fw_length);
+
+end:
+	return rc;
+}
+
+void cam_unreserve_icp_fw(struct cam_fw_alloc_info *icp_fw, size_t fw_length)
+{
+	iounmap(icp_fw->fw_kva);
+}
+
 static int camera_platform_compare_dev(struct device *dev, const void *data)
 {
 	return platform_bus_type.match(dev, (struct device_driver *) data);

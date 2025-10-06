c0336b1272bc349903a0107a9a1466214a2396ef
Pavel Dubrova <pashadubrova@gmail.com>
2025-10-06 16:37:53 +0300

    uapi: Move headers to camera subfolder
    Move headers to camera subfolder. This prevents the camera
    UAPI headers from overwriting upstream kernel headers.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- drivers/cam_isp/isp_hw_mgr/isp_hw/include/cam_isp_hw.h
+++ drivers/cam_isp/isp_hw_mgr/isp_hw/include/cam_isp_hw.h
@@ -15,10 +15,10 @@
 
 #include <linux/completion.h>
 #include "cam_hw.h"
-#include <uapi/media/cam_isp.h>
+#include <media/cam_isp.h>
 #include "cam_soc_util.h"
 #include "cam_irq_controller.h"
-#include <uapi/media/cam_isp.h>
+#include <media/cam_isp.h>
 
 #define CAM_ISP_FPS_60                           60
 
